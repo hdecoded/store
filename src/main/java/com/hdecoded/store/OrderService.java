00f4248522d98c9c6240ff38d202e9c141981583
hdecoded <hdecoded@yahoo.com>
2025-04-09 18:11:22 +0530

    Configuring beans using annotations - no autowired single constructor

--- src/main/java/com/hdecoded/store/OrderService.java
+++ src/main/java/com/hdecoded/store/OrderService.java
@@ -1,10 +1,12 @@
 package com.hdecoded.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
-
-    public OrderService() {
-    }
 
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
