aa68499b758bfb57aec18b3c211a65c00ca20e88
hdecoded <hdecoded@yahoo.com>
2025-04-09 17:48:46 +0530

    Setter Injection

--- src/main/java/com/hdecoded/store/OrderService.java
+++ src/main/java/com/hdecoded/store/OrderService.java
@@ -3,11 +3,18 @@ package com.hdecoded.store;
 public class OrderService {
     private PaymentService paymentService;
 
+    public OrderService() {
+    }
+
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
