e97c4d7d1e75963fef90a5d9468e3dcea78b4736
hdecoded <hdecoded@yahoo.com>
2025-04-09 10:23:20 +0000

    Tightly coupled StripePaymentService

---
+++ src/main/java/com/hdecoded/store/StripePaymentService.java
@@ -0,0 +1,8 @@
+package com.hdecoded.store;
+
+public class StripePaymentService {
+    public void processPayment(double amount) {
+        System.out.println("STRIPE PAYMENT SERVICE");
+        System.out.println("Amount: " + amount);
+    }
+}
