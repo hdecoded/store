869ed6ae5a13565804a082f1114110c1599670c5
hdecoded <hdecoded@yahoo.com>
2025-04-09 03:56:32 +0000

    Initial commit

---
+++ src/test/java/com/hdecoded/springbootstore2/SpringBootStore2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hdecoded.springbootstore2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStore2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
