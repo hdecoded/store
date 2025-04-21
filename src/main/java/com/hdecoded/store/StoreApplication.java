e02d4c186364a09f06f6e6bade3f0f8fca52c62f
hdecoded <hdecoded@yahoo.com>
2025-04-21 08:26:44 +0530

    Creating database store_temp using hibernate.

--- src/main/java/com/hdecoded/store/StoreApplication.java
+++ src/main/java/com/hdecoded/store/StoreApplication.java
@@ -1,56 +1,13 @@
 package com.hdecoded.store;
 
-import com.hdecoded.store.entities.*;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-        var user = User.builder()
-                .id(1L)
-                .name("hdecoded")
-                .email("hd@hdecoded.com")
-                .password("password")
-                .build();
-
-        var address = Address.builder()
-                .street("street")
-                .city("city")
-                .state("state")
-                .zip(500081)
-                .build();
-
-        user.addAddress(address);
-
-        user.addTag("tag1");
-
-        var profile = Profile.builder()
-                .id(1L)
-                .bio("bio text")
-                .date_of_birth(LocalDate.parse("2003-08-28"))
-                .build();
-        user.addProfile(profile);
-
-        System.out.println(user);
-
-        var category = Category.builder()
-                .id(1L)
-                .name("category1")
-                .build();
-        var product = Product.builder()
-                .id(1L)
-                .name("hdecoded")
-                .price(BigDecimal.valueOf(45.00))
-                .build();
-
-        category.addProduct(product);
-        System.out.println(category);
+        SpringApplication.run(StoreApplication.class, args);
     }
 
 }
